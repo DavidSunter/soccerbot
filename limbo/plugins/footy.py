5d7aa62f9061d97027fbba34bf1521a942995ad7
John Arnfield <john.arnfield@skybettingandgaming.com>
2016-04-12 15:06:25 +0100

    stuff

--- limbo/plugins/footy.py
+++ limbo/plugins/footy.py
@@ -5,7 +5,9 @@ import os
 import textwrap
 
 
-data = shelve.open("/tmp/footy")
+last_user_fail = None
+
+data = shelve.open("/home/john/.footybot")
 if 'players' not in data:
     data['players'] = []
 if 'date' not in data:
@@ -43,6 +45,7 @@ def footy_remove_player(username):
         return "There are no upcoming games"
     num_players_before = len(data['players'])
     data['players'] = [player for player in data['players'] if player != username]
+    data['teams'] = [[player for player in team if player != username] for team in data['teams']]
     num_players_after = len(data['players'])
     if num_players_before != num_players_after:
         return "{} is no longer playing in the game on {}".format(username, data['date'])
@@ -55,8 +58,9 @@ def footy_set_team(username, team):
         return "There are no upcoming games"
     if username not in data['players']:
         return "{} is not down to play in the game on {}".format(username, data['date'])
-    data['teams'] = [[player for player in team if username != player] for team in data['teams']]
-    data['teams'][team - 1] += [username]
+    data['teams'] = [
+        [player for player in _team if username != player or _team_id + 1 == team]
+        for _team_id, _team in enumerate(data['teams'])]
     return "{} added to team {}".format(username, team)
 
 
@@ -66,6 +70,11 @@ def footy_set_teams(team_a, team_b):
     for player in team_a + team_b:
         if player not in data['players']:
             return "{} is not down to play on {}".format(player, data['date'])
+    duplicates = set(team_a).intersection(set(team_b))
+    if duplicates:
+        return "{} {} defined in both teams".format(
+            ", ".join(duplicates),
+            "is" if len(duplicates) == 1 else "are")
     data['teams'] = [team_a, team_b]
     teams = " vs ".join([", ".join(team) for team in data['teams']])
     return "The teams are {}".format(teams)
@@ -76,7 +85,7 @@ def footy_get():
         return "There are no upcoming games"
     information = ["Next game set for {}".format(data['date'])]
     if any(data['teams']):
-        teams = " vs ".join([", ".join(team) for team in data['teams']])
+        teams = " vs ".join([", ".join(team) or "nobody" for team in data['teams']])
         information.append("The teams are {}".format(teams))
         unallocated_players = set(data['players']) - set([player for team in data['teams'] for player in team])
         if len(unallocated_players) > 1:
@@ -100,6 +109,8 @@ def footy_done():
         return "There are no upcoming games"
     data['date'] = None
     data['players'] = []
+    data['teams'] = [[], []]
+    data['limit'] = None
     return "Game removed"
 
 
@@ -156,6 +167,8 @@ def get_username(user):
 
 
 def on_message(msg, server):
+    global last_user_fail
+
     text = msg.get("text", "")
     match = re.match(r"!footy( .*)?", text)
     if not match:
@@ -166,49 +179,68 @@ def on_message(msg, server):
     # set <date>
     match = re.match(r"^set (.+?)(\s(\d+))?\s*$", command)
     if match:
+        last_user_fail = None
         date = match.group(1)
         limit = match.group(3)
         return footy_set_date(date, int(limit) if limit is not None else 10)
 
     # add <username>
-    match = re.match(r"^add (.+)", command)
+    match = re.match(r"^add ([a-z0-9][a-z0-9._-]*)", command)
     if match:
+        last_user_fail = None
         return footy_add_player(match.group(1))
 
     # remove <username>
-    match = re.match(r"^remove (.+)", command)
+    match = re.match(r"^remove ([a-z0-9][a-z0-9._-]*)", command)
     if match:
+        last_user_fail = None
         return footy_remove_player(match.group(1))
 
     # teams <name[, name ...]> vs <name[, name ...]>
     match = re.match(r"^teams (.+) vs (.+)", command)
     if match:
+        last_user_fail = None
         teams = [re.findall("[a-z0-9][a-z0-9._-]*", team) for team in [match.group(1), match.group(2)]]
         return footy_set_teams(*teams)
 
     # team <name> <side>
-    match = re.match(r"^team (.+) ([12])", command)
+    match = re.match(r"^team ([a-z0-9][a-z0-9._-]*) ([12])", command)
     if match:
+        last_user_fail = None
         return footy_set_team(match.group(1), int(match.group(2)))
 
     # join
     match = re.match(r"^join", command)
     if match:
+        last_user_fail = None
         return footy_join(get_username(msg['user']))
 
     # leave
     match = re.match(r"^leave", command)
     if match:
+        last_user_fail = None
         return footy_leave(get_username(msg['user']))
 
     # done
     match = re.match(r"^done", command)
     if match:
+        last_user_fail = None
         return footy_done()
 
     # help
     match = re.match(r"^help", command)
     if match:
+        last_user_fail = None
         return footy_help()
 
-    return footy_get()
+    match = re.match(r"^\s*$", command)
+    if match:
+        last_user_fail = None
+        return footy_get()
+
+    if msg['user'] == last_user_fail:
+        return ":unamused:"
+
+    last_user_fail = msg['user']
+    return "Don't know how to {}".format(command)
+
